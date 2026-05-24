4a6cbc174188581126d8465e1f443e4a49337306
MakoBoko <mboskovic420@gmail.com>
2026-05-24 17:24:51 +0200

    Add Exercise 1.19

---
+++ Chapter 1 - Building Abstractions With Procedures/exrc_1.19.scm
@@ -0,0 +1,28 @@
+#lang sicp
+
+(define (fib n)
+  (fib-iter 1 0 0 1 n))
+
+(define (even? a) (= (remainder a 2) 0))
+
+(define (square x) (* x x))
+
+(define (fib-iter a b p q count)
+  (cond ((= count 0) b)
+	((even? count) 
+	 (fib-iter a
+		   b
+		   (+ (square p) (square q)) 
+		   (+ (* 2 p q) (square q)) 
+		   (/ count 2)
+		   ))
+	(else (fib-iter (+ (* b q) (* a q) (* a p))
+			(+ (* b p) (* a q))
+			p
+			q
+			(- count 1)))))
+
+(fib 5)
+(fib 7)
+(fib 8)
+(fib 21)
