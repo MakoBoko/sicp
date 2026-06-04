ecd66d85a9a89421c98ee906f26eb4f9a4562bf6
MakoBoko <mboskovic420@gmail.com>
2026-06-04 22:50:08 +0200

    Add Exercise 1.22

---
+++ Chapter 1 - Building Abstractions With Procedures/exrc_1.22.scm
@@ -0,0 +1,46 @@
+#lang sicp
+
+(define (square x) 
+  (* x x))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+    (report-prime (- (runtime) start-time))))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (search-for-primes p n)
+  (cond 
+    ((= n 0) (newline) (display "FINISHED"))
+    ((even? p) (search-for-primes (+ p 1) n))
+    ((prime? p) (timed-prime-test p) (search-for-primes (+ p 2) (- n 1)))
+    (else (search-for-primes (+ p 2) n ))))
+
+(search-for-primes 1000 3)
+(search-for-primes 10000 3)
+(search-for-primes 100000 3)
+(search-for-primes 1000000 3)
