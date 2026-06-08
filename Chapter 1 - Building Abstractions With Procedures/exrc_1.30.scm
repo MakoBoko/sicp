9534dc1f1c4a737256c2bd78533813bcddc540fc
MakoBoko <mboskovic420@gmail.com>
2026-06-08 22:19:45 +0200

    Add Exercise 1.30

---
+++ Chapter 1 - Building Abstractions With Procedures/exrc_1.30.scm
@@ -0,0 +1,14 @@
+#lang sicp
+
+(define (sum term a next b) 
+  (define (iter a result) 
+    (if (> a b)
+      result
+      (iter (next a) (+ result (term a)))))
+  (iter a 0))
+
+(define (cube x) (* x x x))
+(define (inc x) (+ x 1))
+
+(sum cube 0 inc 3)
+(sum cube 1 inc 10)
