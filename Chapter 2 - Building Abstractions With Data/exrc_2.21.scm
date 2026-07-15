30fa74d04bcdcfbe1961d5e8fefdaa6e55117ef5
MakoBoko <mboskovic420@gmail.com>
2026-07-15 21:33:54 +0200

    Add Exercise 2.21

---
+++ Chapter 2 - Building Abstractions With Data/exrc_2.21.scm
@@ -0,0 +1,15 @@
+#lang sicp
+
+(define (square x) (* x x))
+
+(define (square-list-1 items)
+  (if (null? items)
+      nil
+      (cons (square (car items))
+            (square-list-1 (cdr items)))))
+
+(define (square-list-2 items)
+  (map square items))
+
+(square-list-1 (list 1 2 3 4))
+(square-list-2 (list 1 2 3 4))
