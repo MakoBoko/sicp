bb0c2f242e99716715ce00a6b497143fdc46067f
MakoBoko <mboskovic420@gmail.com>
2026-07-29 22:36:11 +0200

    Add Exercise 2.31

---
+++ Chapter 2 - Building Abstractions With Data/exrc_2.31.scm
@@ -0,0 +1,19 @@
+#lang sicp
+
+(define (square x) (* x x))
+
+(define (square-tree tree)
+  (tree-map square tree))
+
+(define (tree-map f tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (tree-map f sub-tree)
+             (f sub-tree)))
+       tree))
+
+(define test-list (list 1
+                        (list 2 (list 3 4) 5)
+                        (list 6 7)))
+
+(square-tree test-list)
