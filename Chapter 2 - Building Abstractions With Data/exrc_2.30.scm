0998cf0ac3301eb1d7e7d26485bc6eed3f63419d
MakoBoko <mboskovic420@gmail.com>
2026-07-29 22:28:34 +0200

    Add Exercise 2.30

---
+++ Chapter 2 - Building Abstractions With Data/exrc_2.30.scm
@@ -0,0 +1,23 @@
+#lang sicp
+
+(define (square x) (* x x))
+
+(define (square-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (square tree))
+        (else (cons (square-tree (car tree))
+                    (square-tree (cdr tree))))))
+
+(define (square-tree-map tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (square-tree-map sub-tree)
+             (square sub-tree)))
+       tree))
+
+(define test-list (list 1
+                        (list 2 (list 3 4) 5)
+                        (list 6 7)))
+
+(square-tree test-list)
+(square-tree-map test-list)
