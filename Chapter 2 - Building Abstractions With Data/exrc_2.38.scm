e8092028d54e6461fa518013fa02791127c1d83e
MakoBoko <mboskovic420@gmail.com>
2026-08-01 15:05:35 +0200

    Add Exercise 2.38

---
+++ Chapter 2 - Building Abstractions With Data/exrc_2.38.scm
@@ -0,0 +1,26 @@
+#lang sicp
+
+(define (fold-right op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (fold-right op initial (cdr sequence)))))
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(fold-right / 1 (list 1 2 3))
+(fold-left / 1 (list 1 2 3))
+(fold-right list nil (list 1 2 3))
+(fold-left list nil (list 1 2 3))
+
+;; op - should be associatvie in order for fold-right and fold-left to produce the same values
+(fold-right + 1 (list 1 2 3))
+(fold-left + 1 (list 1 2 3))
+(fold-right * 1 (list 1 2 3))
+(fold-left * 1 (list 1 2 3))
